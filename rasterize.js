7ca9691dc83e27b820967198c5007ed92d9550a3
Keang <skeang@gmail.com>
2015-01-16 14:43:00 +0800

    Update s3 library.

--- rasterize.js
+++ rasterize.js
@@ -1,22 +1,32 @@
 var page = require('webpage').create();
 var system = require('system');
+var c = console;
 
 if (system.args.length < 3) {
     console.log('Usage: rasterize.js URL filename');
     phantom.exit(1);
 } else {
     var address = system.args[1];
-    var output = system.args[2];
+    var output_file = system.args[2];
 
+    page.viewportSize = { width:1920, height:1080 }
     page.open(address, function (status) {
         if (status !== 'success') {
-            console.log('Unable to load the address!');
+            c.log('Unable to load the address!');
             phantom.exit();
         } else {
-            window.setTimeout(function () {
-                page.render(output);
+            if(window.document.readyState == "complete"){
+                //TODO: add format, quality
+                c.log("Rendering");
+                page.render( output_file, { format:'png', quality:'100' } )
                 phantom.exit();
-            }, 200);
+            }else{
+                window.onload = function () {
+                    c.log("Rendering");
+                    page.render( output_file, { format:'png', quality:'100' } )
+                    phantom.exit();
+                };
+            }
         }
     });
 }
